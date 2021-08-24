8b1ad11b5f2cb682a708172c1ab0678df82bf409
Ryan Clare <52092909+rmclare445@users.noreply.github.com>
2021-08-23 17:44:33 -0700

    Update and rename pixels.py to pixel.py
    Switch module name and variable name

--- pixel.py
+++ pixel.py
@@ -8,9 +8,9 @@ pixels = neopixel.NeoPixel( gpio_pin, num_pixels )
 
 def pixel_sun( pixnum, value ):
     if value == True:
-        pixel[pixnum] = (0, 0, 255)
+        pixels[pixnum] = (0, 0, 255)
     else:
-        pixel[pixnum] = (255, 0, 150)
+        pixels[pixnum] = (255, 0, 150)
         
 def pixel_I_frac( pixnum, value ):
     return
